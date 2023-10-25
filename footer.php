c1c2e7a375d7f91795ec1b80ca1235e9efcee9be
Emil Wibe <mail@emilwibe.dk>
2023-10-25 11:35:41 +0200

    [copywriting] colophon

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+  <footer>
+    <p class="colophon">Snippets.dk er udviklet af <a href="https://ew.dk" target="_blank">EW Web</a> &copy; 2019 - <?php echo date("Y"); ?></p>
+  </footer>
+
   <?php wp_footer(); ?>
 
   <script>//hljs.initHighlightingOnLoad();</script>
